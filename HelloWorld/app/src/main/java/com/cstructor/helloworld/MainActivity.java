c24c02e1f65bec0f0db6ec3fd9db86a2670b511a
Dan Vallejo <danv@cstructor.com>
2016-10-18 20:27:20 -0700

    Session 3 up to activity

--- HelloWorld/app/src/main/java/com/cstructor/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/cstructor/helloworld/MainActivity.java
@@ -2,6 +2,9 @@ package com.cstructor.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("MainActivity", "onCreate");
     }
+
+   public void sendMessage(View view) throws InterruptedException {
+
+       if (view.getId() == R.id.uxImageButton){
+           Toast.makeText(this, "uxImageButton was clicked!", Toast.LENGTH_LONG).show();
+           Thread.sleep(10000);
+       }
+       else if (view.getId() == R.id.uxImageTextButton){
+           Toast.makeText(this, "uxImageTextButton was clicked!", Toast.LENGTH_LONG).show();
+       }
+       else if (view.getId() == R.id.uxTextButton){
+           Toast.makeText(this, "uxTextButton was clicked!", Toast.LENGTH_LONG).show();
+       }
+       else{
+           Toast.makeText(this, "Something weird was clicked!", Toast.LENGTH_LONG).show();
+       }
+   }
 }
